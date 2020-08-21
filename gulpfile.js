041ef350d804e54a1d3243a7bc7dee08728e6954
Dmytro <gectorweb@gmail.com>
2020-08-21 20:23:29 +0300

    Basic tasks for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const {src, dest, series, watch} = require('gulp')
-const sass = require('gulp-sass')
-const csso = require('gulp-csso')
-const include = require('gulp-file-include')
-const htmlmin = require('gulp-htmlmin')
-const del = require('del')
-const concat = require('gulp-concat')
-const autoprefixer = require('gulp-autoprefixer')
-const sync = require('browser-sync').create()
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const include = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const sync = require('browser-sync').create();
 
 function html() {
   return src('src/**.html')
     .pipe(include({
       prefix: '@@'
     }))
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
+    // .pipe(htmlmin({
+    //   collapseWhitespace: true
+    // }))
     .pipe(dest('dist'))
 }
 
@@ -26,8 +26,22 @@ function scss() {
       browsers: ['last 2 versions']
     }))
     .pipe(csso())
-    .pipe(concat('index.css'))
-    .pipe(dest('dist'))
+    .pipe(concat('style.min.css'))
+    .pipe(dest('dist/css'))
+}
+
+function js() {
+  return src('src/js/**.js')
+    .pipe(dest('dist/js'))
+}
+
+function img() {
+  return src('src/images/**/*.*')
+    .pipe(dest('dist/images'))
+}
+function fonts() {
+  return src('src/fonts/**/*.*')
+    .pipe(dest('dist/fonts'))
 }
 
 function clear() {
@@ -37,12 +51,13 @@ function clear() {
 function serve() {
   sync.init({
     server: './dist'
-  })
+  });
 
-  watch('src/**.html', series(html)).on('change', sync.reload)
-  watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+  watch('src/**.html', series(html)).on('change', sync.reload);
+  watch('src/scss/**.scss', series(scss)).on('change', sync.reload);
+  watch('src/js/**.js', series(js)).on('change', sync.reload);
 }
 
-exports.build = series(clear, scss, html)
-exports.serve = series(clear, scss, html, serve)
-exports.clear = clear
+exports.build = series(clear, scss, html, js, img, fonts);
+exports.serve = series(clear, scss, html, js, img, fonts, serve);
+exports.clear = clear;
